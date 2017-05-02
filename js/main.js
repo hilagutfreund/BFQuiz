b441d5e374c35da64ea4b1b89b23413d56ada4da
hilagutfreund <hila.gut@gmail.com>
2017-05-02 14:13:07 -0700

    figuring out how to get web data

--- js/main.js
+++ js/main.js
@@ -30,6 +30,38 @@ var bfApp = angular.module('bfApp', ['ngRoute', 'ui.router']);
 
         })
 
+        // jQuery(function($) {
+        //   $('.instagram').on('willLoadInstagram', function(event, options) {
+        //   });
+        //   $('.instagram').on('didLoadInstagram', function(event, response) {
+        //     $scope.images = response['data']; 
+        //     $rootScope.$broadcast('photosready', $scope.images);
+        //   });
+        //  $('.instagram').instagram({
+        //           userId: 3271830693,
+        //           accessToken: '3271830693.e8750bb.cb4e791f241b474d891c043abdac34c0'
+        //         });
+
+        //     });
+
+//  $(document).ready(function () {
+//             var url = "https://www.buzzfeed.com/spreetsg/pick-gear-from-serengetee-and-well-tell-you-the-n-ge2c?utm_term=.pkwL4g5q7#.uq1LdM809";
+// $.ajaxPrefilter( function (options) {
+//   if (options.crossDomain && jQuery.support.cors) {
+//     var http = (window.location.protocol === 'http:' ? 'http:' : 'https:');
+//     options.url = http + '//cors-anywhere.herokuapp.com/' + options.url;
+//     //options.url = "http://cors.corsproxy.io/url=" + options.url;
+//   }
+// });
+
+// $.get(
+//    url,
+//     function (response) {
+//         console.log(response);
+
+// });
+//     });
+
         $scope.myprojects = [
             {
                 "title": "WDJC", 
