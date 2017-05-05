6552a22e803ae3751a532b1def43faccf648b56d
hilagutfreund <hila.gut@gmail.com>
2017-05-05 00:03:35 -0700

    finished! Validation worksgit add -A

--- js/main.js
+++ js/main.js
@@ -25,6 +25,18 @@ var bfApp = angular.module('bfApp', ['ngRoute', 'ui.router', 'ngSanitize']);
         //     return $sce.trustAsHtml(html_code);
         // };
         $scope.quizit = "";
+        $scope.buzzfeedURLPattern = new RegExp("https?:\/\/(www\.)buzzfeed.com([-a-zA-Z0-9@:%_\+.~#?&//=]*)", "i");
+        // $scope.buzzfeedURLPattern = (function() {
+        //     var regexp = /^\(?(\d{3})\)?[ .-]?(\d{3})[ .-]?(\d{4})$/;
+        //     return {
+        //         test: function(value) {
+        //             if( $scope.requireTel === false ) {
+        //                 return true;
+        //             }
+        //             return regexp.test(value);
+        //         }
+        //     };
+        // })();
         /**
         * Randomize array element order in-place.
         * Using Durstenfeld shuffle algorithm.
@@ -60,6 +72,7 @@ $scope.shuffledBadges = shuffleArray($scope.badges);
 $scope.searchUrl = function(url){
     $scope.origresultList = []; 
     $scope.actualResultsList = [];
+    $scope.personalityQuiz = false; 
     console.log(url);
     jQuery(function($){
     console.log("we're here!");
@@ -71,7 +84,7 @@ $scope.searchUrl = function(url){
     //options.url = "http://cors.corsproxy.io/url=" + options.url;
         }
     });
-
+   
     $.get(
         url,
         function (response) {
@@ -83,19 +96,43 @@ $scope.searchUrl = function(url){
             $scope.origresultsList = ($(html).find('article'));
             for(i=0; i<$scope.origresultsList.length; i++){
                 var result = $scope.origresultsList[i];
+                console.log(result);
                 if(($(result).attr('class')) == 'subbuzz-quiz__result js-subbuzz-quiz__result xs-mb3 js-hidden'){
                     $("figure", result).remove().end()[0];
-                
                     $scope.actualResultsList.push({html:result, image: $scope.shuffledBadges[i%($scope.shuffledBadges.length)]}); 
-                }
-            }
-            //find title and subtitle
-            $scope.headerList = ($(html).find('hgroup'));
-            $("div", $scope.headerList[0]).remove().end()[0];
+                
 
-            //broadcast
-            $rootScope.$broadcast('titleready', $scope.headerList[0]);
-            $rootScope.$broadcast('answersready', $scope.actualResultsList);
+                // }else if(($(result).attr('class')) == 'subbuzz subbuzz-quiz subbuzz-quiz--numbered subbuzz-quiz--personality js-unsupported xs-mb4'){
+                //     console.log("it's true!");
+                //     $scope.personalityQuiz = true; 
+                }
+                console.log($scope.actualResultsList);
+            }
+
+
+
+            $scope.personalityList = ($(html).find('div'));
+            for(i=0; i<$scope.personalityList.length; i++){
+                var personality = $scope.personalityList[i];
+                if(($(personality).attr('class')) == 'subbuzz subbuzz-quiz subbuzz-quiz--numbered subbuzz-quiz--personality js-unsupported xs-mb4'){
+                    $scope.personalityQuiz = true; 
+                    console.log("it's true!");
+                }        
+            }
+
+            if($scope.personalityQuiz == true){
+                //find title and subtitle
+                $scope.headerList = ($(html).find('hgroup'));
+                $("div", $scope.headerList[0]).remove().end()[0];
+
+                //broadcast
+                $rootScope.$broadcast('titleready', $scope.headerList[0]);
+                $rootScope.$broadcast('answersready', $scope.actualResultsList);
+            }else{
+                $rootScope.$broadcast('titleready', {innerHTML: "<h2>Unforunately, this type of Quiz or Article is unsupported. Please try another one!"});
+                $rootScope.$broadcast('answersready', []);
+
+            }
 
 
     });
